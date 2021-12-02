63ead3a45a515ec0e29e43187fb692037d3991c6
virendrasingrr <72337987+virendrasingrr@users.noreply.github.com>
2021-12-02 09:55:00 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,10 +2,10 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "2105608519:AAFizUcSgA5huSRgWrbGcxQN4oVpRcJiigs")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("APP_ID", 17957769))
+    API_HASH = os.environ.get("0cfdea5ed128ed41fa08bc18a5eec6a2")
     # Get these values from my.telegram.org
     # Banned Unwanted Members..
     BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
@@ -29,8 +29,8 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     # your telegram id
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    OWNER_ID = int(os.environ.get("OWNER_ID", "1159374942"))
     # database session name, example: xurluploader
     SESSION_NAME = os.environ.get("SESSION_NAME", "")
     # database uri (mongodb)
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://viru:Yfd8kTDV56A@cluster0.rklc4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
